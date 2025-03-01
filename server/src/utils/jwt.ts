77aab879a7f80fcc6f2d258aee460a3c769657e6
hens-msn <hendymamusung@outlook.com>
2025-03-02 06:11:03 +0700

    fix: handleauth invalid token

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -15,7 +15,7 @@ if (!JWT_SECRET) {
 }
 
 export const generateToken = (payload: Payload) => {
-  return jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: "1m" });
 };
 
 export const verifyToken = async (token: string) => {
